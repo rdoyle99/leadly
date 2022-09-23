2bcb5edcb50c75dbc80b463dd03a4f11b7fb9701
rdoyle99 <rpdoyle1@gmail.com>
2022-09-23 14:04:13 -0500

    SEO OG

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,32 @@ import Knowledge from "@/components/Knowledge";
 import Leads from "@/components/Leads";
 import Form from "@/components/Form";
 import Footer from "@/components/Footer";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
     <div className="bg-bluish text-white">
+      <NextSeo
+        title="Damn Good Leads"
+        description="Curated lead lists from the best sales and scraping minds in the market."
+        canonical="https://www.damngoodleads.com"
+        openGraph={{
+          url: "https://www.damngoodleads.com",
+          title: "Damn Good Leads",
+          description:
+            "Curated lead lists from the best sales and scraping minds in the market.",
+          images: [
+            {
+              url: "https://i.imgur.com/P6QcGAD.png",
+              width: 800,
+              height: 600,
+              alt: "OG Image for Damn Good Leads",
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "Damn Good Leads",
+        }}
+      />
       <Header />
 
       <Leads />
